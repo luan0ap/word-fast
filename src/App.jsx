5fe9b97813d4bcdd30e8532b5b0a1e8212a7136d
luan0ap <luanap.kun@gmail.com>
2018-11-09 15:56:31 -0200

    remove input time and score, create timer function

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,36 @@ import Header from './components/Header'
 import './style.css'
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       words: [],
-      current: '',
+      randomWord: '',
       time: 3,
       score: 0
     }
+
+    this.$inputTimer = React.createRef()
   }
 
-  componentDidMount () {
+  componentDidMount() {
     getWords().then(({ data }) => this.setState(({ words, current }) => ({
       words: [...data.words],
-      current: randomItem([...data.words])
+      randomWord: randomItem([...data.words])
     })))
   }
 
-  verifyWord = () => {
-     
+  timer = (remaning) => {
+    if (remaning) {
+      this.setState({ time: this.state.time - 1 })
+      setTimeout(this.timer, 1000, remaning - 1)
+    }
+  }
+
+  startChallenge = ({ target }) => {
+    const { randomWord } = this.state
+
+    this.timer(this.state.time)
   }
 
   render() {
@@ -40,18 +51,18 @@ class App extends Component {
         <main className='content-game'>
 
           <section className='inputs-words'>
-            <input type='text' disabled='disabled' className='words' value={this.state.current} />
+            <input type='text' disabled='disabled' className='words' value={this.state.randomWord} />
 
-            <input type='text' className='send' onKeyDown={this.verifyWord} />
+            <input type='text' className='send' onChange={this.startChallenge} />
           </section>
 
           <section className='timer-score'>
 
-            <label htmlFor='timer'>Time left: </label>
-            <input type='time' name='timer' id='timer' className='timer' value='00:04' disabled />
+            <span>Time left: </span>
+            <span className='timer'>{this.state.time}</span>
 
-            <label htmlFor="score">Score: </label>
-            <input type='number' name='score' id='score' value='0' disabled />
+            <span>Score: </span>
+            <span className="score">{this.state.score}</span>
 
           </section>
         </main>
--- src/components/Input/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'feact'
-
-const Input = () => {}
-
-export default Input
